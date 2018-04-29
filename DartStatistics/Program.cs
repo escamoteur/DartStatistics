b72452fb20403d4acf5c56756dee7134b3e44c35
escamoteur <github@burkharts.net>
2018-04-29 18:36:08 +0200

    Small bug fix

--- DartStatistics/Program.cs
+++ DartStatistics/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -16,14 +17,16 @@ namespace DartStatistics
         private static int lineCountNetto  = 0;
         private static int lineCountBrutto = 0;
         private static int classCount = 0;
-        
+        private static int charCount = 0;
+
+
         static void Main(string[] args)
         {
             var currentDir = Directory.GetCurrentDirectory();
 
             Count(currentDir);
             Console.WriteLine("Total:");
-            Console.WriteLine("LinesBrutto: " + lineCountBrutto + " - LinesNetto: " + lineCountNetto + " - Classes: " + classCount);
+            Console.WriteLine("LinesBrutto: " + lineCountBrutto + " - LinesNetto: " + lineCountNetto + " - Code chars: " + charCount + " - Classes: " + classCount);
 
 
             Console.ReadLine();
@@ -37,14 +40,18 @@ namespace DartStatistics
                 int _lineCountBrutto = 0;
                 int _lineCountNetto = 0;
                 int _classCount = 0;
+                int _charCount = 0;
                 Console.WriteLine(name);
+
 
                 var reader =  File.OpenText(name);
 
                 var line = reader.ReadLine();
+                var lineCharCount = 0;
                 while (line != null)
                 {
-                    if (string.IsNullOrWhiteSpace(line))
+                    lineCharCount = CountValidChars(line);
+                    if (lineCharCount == 0)
                     {
                         lineCountBrutto++;
                     }
@@ -58,11 +65,13 @@ namespace DartStatistics
                         }
                     }
                     line = reader.ReadLine();
+                    _charCount += lineCharCount;
                 }
-                Console.WriteLine("LinesBrutto: " + _lineCountBrutto + " - LinesNetto: " + _lineCountNetto + " - Classes: " + _classCount);
+                Console.WriteLine("LinesBrutto: " + _lineCountBrutto + " - LinesNetto: " + _lineCountNetto + " - Code chars: " + _charCount + " - Classes: " + _classCount);
                 lineCountNetto += _lineCountNetto;
                 lineCountBrutto += _lineCountBrutto;
                 classCount += _classCount;
+                charCount += _charCount;
             }
 
             var dirNames = Directory.EnumerateDirectories(path);
@@ -74,6 +83,26 @@ namespace DartStatistics
 
 
             
+        }
+
+        static int CountValidChars(string s)
+        {
+            int count=0;
+            for (var index = 0; index < s.Length; index++)
+            {
+                char c = s[index];
+                char next = index + 1 < s.Length - 1 ? s[index + 1] : ' ';
+
+                if (c == '/' && next == '/') // found comment
+                {
+                    return count;
+                }
+                if (!char.IsWhiteSpace(c))
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
